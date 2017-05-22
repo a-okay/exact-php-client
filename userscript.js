90b1e5adebe78d676c02d4b5dcd5870f36857fc5
Karel van IJperen <hjk010@gmail.com>
2017-05-23 00:26:07 +0200

    Improve userscript.js

--- userscript.js
+++ userscript.js
@@ -9,26 +9,33 @@
 
 (function() {
   var instant_download = false;
+  var debug = false;
 
-  /** Add textarea to display php code **/
-  // Push current content to the left.
-  $('body > form').css('float', 'left');
-  $('body > form').css('width', '50%');
-  $('body > form').css('overflow', 'auto');
-
-  // Add textarea to body, css might need some work.
+  /** Adding requred ellements **/
+  // Add download button and show it if prefered.
   $('<a />', {
     id : 'php_download',
     text: 'Download Code',
-    style: 'margin: 10px; padding: 10px; color: white; background-color: red'
-  }).appendTo('body');
+    style: 'margin: 10px; padding: 10px; color: white; background-color: red; display: none;'
+  }).prependTo('body');
 
-  $('<br /><br />').appendTo('body');
+  if (instant_download === false) {
+    $('<br /><br />').insertAfter('#php_download');
+    $('#php_download').show();
+  }
 
-  $('<textarea/>', {
-    id : 'php',
-    style: 'min-height:800px; width: 47%; margin-left:10px;'
-  }).appendTo('body');
+  // Add textarea for instant results when debuging.
+  if (debug) {
+    // Push current content to the left.
+    $('body > form').css('float', 'left');
+    $('body > form').css('width', '50%');
+    $('body > form').css('overflow', 'auto');
+
+    $('<textarea/>', {
+      id : 'php_debug',
+      style: 'min-height:800px; width: 47%; margin-left:10px;'
+    }).appendTo('body');
+  }
 
   /** Initialize variables **/
   var primarykey = 'ID';
@@ -36,14 +43,14 @@
   // Fetch entity URL and strip '/api/v1/{division}/'
   var url = $('#serviceUri').text().replace(/^\/api\/v[0-9]\/[^/]+\//, '');
   var classname = url.replace(/.+\/(.+?)s?$/,'$1'); // Last part after slash should be the (plural) classname.
-  
+
   /** Fetch attribute information **/
   $('#referencetable tr input').each(function() {
     data[$(this).attr('name')] = {
       'type' : $(this).attr('data-type').replace('Edm.', ''),
       'description' : $(this).parent().siblings('td:last-child').text().trim()
     };
-    
+
     // Set primarykey when found. Should be first itteration.
     if ($(this).attr('data-key') == "True") {
       primarykey = $(this).attr('name');
@@ -52,17 +59,17 @@
 
   /** Build php code **/
   phptxt = "<?php namespace Picqer\\Financials\\Exact;\n\n/**";
-  
+
   // Build docblock
   phptxt += "\n * Class " + classname;
   phptxt += "\n *\n * @package Picqer\\Financials\\Exact";
   phptxt += "\n * @see " + window.location.href;
   phptxt += "\n *";
-  $.each(data,function(attribute, info){
+  $.each(data, function(attribute, info){
     phptxt += "\n * @property " + info.type + " $" + attribute + " " + info.description;
   });
   phptxt += "\n */";
-  
+
   // Build class
   phptxt += "\nclass " + classname + " extends Model\n{\n\n    use Query\\Findable;\n    use Persistance\\Storable;";
   if (primarykey != 'ID') {
@@ -75,9 +82,9 @@
   phptxt += "\n    ];";
   phptxt += "\n\n    protected $url = '" + url + "';";
   phptxt += "\n\n}";
-  
+
   // Display php code
-  $('#php').text(phptxt);
+  $('#php_debug').text(phptxt);
 
   // Present the php code as a download
   $('#php_download').attr("href",
